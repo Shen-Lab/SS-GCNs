408dc3d932727d283ea55546d17d26a3fd4fb8ba
yyou1996 <yuning.you@foxmail.com>
2020-02-19 18:27:26 -0600

    graph_completion

--- SS-GCN/main_comp.py
+++ SS-GCN/main_comp.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import numpy as np
 
 import net as net
-from utils import load_data
+from utils import load_data, load_adj_raw, normalize_adj, sparse_mx_to_torch_sparse_tensor
 from sklearn.metrics import f1_score
 
 
@@ -18,28 +18,38 @@ def run(args, seed):
 
     node_num = features.size()[0]
     class_num = labels.numpy().max() + 1
-    idx_unlabel = list(range(class_num*20, node_num))
 
     adj = adj.cuda()
     features = features.cuda()
     labels = labels.cuda()
 
     loss_func = nn.CrossEntropyLoss()
+    loss_func_ss = nn.L1Loss()
     early_stopping = 10
 
-    cluster_labels_file = './cluster_labels/' + args['dataset'] + '.npy'
-    ss_labels = torch.tensor(np.load(cluster_labels_file), dtype=torch.int64).cuda()
-    net_gcn = net.net_gcn_multitask(embedding_dim=args['embedding_dim'], ss_dim=args['embedding_dim'][-1])
+    adj_raw = load_adj_raw(args['dataset']).tocoo()
+    idx_mask = list(range(node_num))
+    adj_mask = adj_raw
+    adj_mask[idx_mask, idx_mask] = 0
+    adj_mask = sparse_mx_to_torch_sparse_tensor(normalize_adj(adj_mask)).cuda()
+
+    reduced_dim = args['reduced_dimension']
+    ss_labels, _, _ = features.svd()
+    ss_labels = ss_labels[:, reduced_dim].cuda()
+
+    net_gcn = net.net_gcn_multitask(embedding_dim=args['embedding_dim'], ss_dim=args['reduced_dimension'])
     net_gcn = net_gcn.cuda()
     optimizer = torch.optim.Adam(net_gcn.parameters(), lr=args['lr'], weight_decay=args['weight_decay'])
-    loss_val = []
-    for epoch in range(1000):
+    best_val = 0
+    best_val_test = 0
+    for epoch in range(500):
 
         optimizer.zero_grad()
-        output, output_ss = net_gcn(features, adj)
+        output, _ = net_gcn(features, adj)
+        _, output_ss = net_gcn(features, adj_mask)
         loss_target = loss_func(output[idx_train], labels[idx_train])
-        loss_ss = loss_func(output_ss[idx_unlabel], ss_labels[idx_unlabel])
-        loss = loss_target * args['loss_weight'] + loss_ss * (1 - args['loss_weight'])
+        loss_ss = loss_func_ss(output_ss, ss_labels) * 1e2
+        loss = loss_target + loss_ss * args['loss_weight']
         # print('epoch', epoch, 'loss', loss_target.data)
         loss.backward()
         optimizer.step()
@@ -47,20 +57,16 @@ def run(args, seed):
         # validation
         with torch.no_grad():
             output, _ = net_gcn(features, adj, val_test=True)
-            loss_val.append(loss_func(output[idx_val], labels[idx_val]).cpu().numpy())
+            # loss_val.append(loss_func(output[idx_val], labels[idx_val]).cpu().numpy())
             # print('val acc', f1_score(labels[idx_val].cpu().numpy(), output[idx_val].cpu().numpy().argmax(axis=1), average='micro'))
 
-        # early stopping
-        if epoch > early_stopping and loss_val[-1] > np.mean(loss_val[-(early_stopping+1):-1]):
-            break
+            acc_val = f1_score(labels[idx_val].cpu().numpy(), output[idx_val].cpu().numpy().argmax(axis=1), average='micro')
+            acc_test = f1_score(labels[idx_test].cpu().numpy(), output[idx_test].cpu().numpy().argmax(axis=1), average='micro')
+            if acc_val > best_val:
+                best_val = acc_val
+                best_val_test = acc_test
 
-    # test
-    with torch.no_grad():
-        output, _ = net_gcn(features, adj, val_test=True)
-        acc_val = f1_score(labels[idx_val].cpu().numpy(), output[idx_val].cpu().numpy().argmax(axis=1), average='micro')
-        acc_test = f1_score(labels[idx_test].cpu().numpy(), output[idx_test].cpu().numpy().argmax(axis=1), average='micro')
-
-    return acc_val, acc_test
+    return best_val, best_val_test
 
 
 def parser_loader():
@@ -69,6 +75,7 @@ def parser_loader():
     parser.add_argument('--embedding-dim', nargs='+', type=int, default=[3703,16,6])
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--weight-decay', type=float, default=5e-4)
+    parser.add_argument('--reduced-dimension', type=int, default=32)
     parser.add_argument('--loss-weight', type=float, default=0.5)
     parser.add_argument('--grid-search', type=bool, default=False)
     return parser
@@ -104,18 +111,22 @@ if __name__ == "__main__":
 
     else:
 
+        pn_list = [8, 9, 10, 11, 12, 13, 14, 15, 16]
         lw_list = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        pn_len = len(pn_list)
         lw_len = len(lw_list)
-        acc_val_table = np.zeros(lw_len)
+        acc_val_table = np.zeros((pn_len, lw_len))
 
-        for j in range(lw_len):
+        for i in range(pn_len):
+            for j in range(lw_len):
 
-            args['loss_weight'] = lw_list[j]
+                args['partitioning_num'] = pn_list[i]
+                args['loss_weight'] = lw_list[j]
 
-            acc_val = np.zeros(10)
-            for seed in range(10):
-                acc_val[seed], _ = run(args, seed)
-            acc_val_table[j] = acc_val.mean()
+                acc_val = np.zeros(10)
+                for seed in range(10):
+                    acc_val[seed], _ = run(args, seed)
+                acc_val_table[i, j] = acc_val.mean()
 
         print('finish')
         print('val mean table', acc_val_table)
